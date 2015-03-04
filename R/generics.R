95018a97d54409acc3c0d3b8d8afb262d6f5182b
Charles Determan <cdetermanjr@hotmail.com>
2015-03-04 11:54:33 -0600

    generics for matrix types (e.g. square, positive-definite)

---
+++ R/generics.R
@@ -0,0 +1,9 @@
+# Generics for various functions
+
+setGeneric("isSquare", function(object){
+  standardGeneric("isSquare")
+})
+
+setGeneric("isPositiveDefinite", function(object, ...){
+  standardGeneric("isPositiveDefinite")
+})
