99e19d94a4e6b022eed8e0c8c53225cab2dc2967
Charles Determan <cdetermanjr@hotmail.com>
2015-03-04 12:37:23 -0600

    added positive-definite assert check

--- R/asserts.R
+++ R/asserts.R
@@ -2,5 +2,12 @@
 #' @import assertive
 assert_is_bigmatrix <- function (x) 
 {
-  assertive:::assert_engine(x, is.big.matrix)
+  assertive:::assert_engine(x, is.big.matrix, "x is not a big.matrix")
+}
+
+#' @export
+assert_is_positive_definite <- function(x)
+{
+  assertive:::assert_engine(x, isPositiveDefinite, 
+                            "matrix is not positive-definite")
 }
