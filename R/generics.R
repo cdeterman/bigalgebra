4fdbc568ada12f1567874e61b748b13c541ab976
Charles Determan <cdetermanjr@hotmail.com>
2015-03-04 12:04:16 -0600

    added note about function location

--- R/generics.R
+++ R/generics.R
@@ -1,5 +1,8 @@
 # Generics for various functions
 
+# These matrix type functions seem like they should be 
+# in bigmemory but internals depend on bigalgebra so they 
+# will remain here for now
 setGeneric("isSquare", function(object){
   standardGeneric("isSquare")
 })
