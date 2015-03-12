3a0a0d158804bfe616205eb049dd69ec2f1036ff
Charles Determan <cdetermanjr@hotmail.com>
2015-03-12 15:08:55 -0500

    created new assert functions for different matrix types, useful for making sure parameters are correct types

--- R/asserts.R
+++ R/asserts.R
@@ -5,9 +5,36 @@ assert_is_bigmatrix <- function (x)
   assertive:::assert_engine(x, is.big.matrix, "x is not a big.matrix")
 }
 
+assert_is_square <- function(x)
+{
+  assertive:::assert_engine(x, isSquare, 
+                            "matrix is not square")
+}
+
 #' @export
 assert_is_positive_definite <- function(x)
 {
   assertive:::assert_engine(x, isPositiveDefinite, 
                             "matrix is not positive-definite")
+}
+
+#' @export
+assert_is_positive_semi_definite <- function(x)
+{
+  assertive:::assert_engine(x, isPositiveSemiDefinite, 
+                            "matrix is not positive semi-definite")
+}
+
+#' @export
+assert_is_negative_definite <- function(x)
+{
+  assertive:::assert_engine(x, isNegativeDefinite, 
+                            "matrix is not negative-definite")
+}
+
+#' @export
+assert_is_negative_semi_definite <- function(x)
+{
+  assertive:::assert_engine(x, isNegativeSemiDefinite, 
+                            "matrix is not negative semi-definite")
 }
